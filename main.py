65b80b8078c86b1a6c67f8a9ca72694031c13db0
aperf3ct <158367984+aperf3ct@users.noreply.github.com>
2024-04-25 22:06:42 +1200

    Update main.py
    making a copy to revert to later if needed

--- main.py
+++ main.py
@@ -7,10 +7,10 @@
 
 '''
 25/04/2024
-3:00pm
-Setup of main menu function and user/pet names
+10pm
+setting up feed function
 '''
-
+# FUNCTIONS
 def main_menu():
   print("\nWhat would you like to do?")
   print("1. Feed")
@@ -29,13 +29,55 @@ def main_menu():
       print("Please enter a valid number.")
 
 
+def user_selection(selection, weight):
+  if selection == 1:
+    weight = feed(weight)
+    return weight
+  elif selection == 2:
+    weight = play()
+    return weight
+  elif selection == 3:
+    edit_names()
+  elif selection == 4:
+    quit()
+
+def feed(weight):
+  print("What woud you like to feed your pet?")
+  print("1. Kale\n2. Broccoli\n3. Apples")
+  Valid = False
+  while not Valid:
+    try:
+      feed_input = int(input("\nEnter your choice: "))
+      if feed_input < 1 or feed_input > 3:
+        print("Please enter a valid number.")
+      else:
+        Valid = True
+        if feed_input == 1:
+          weight = weight + 0.1
+          print("You fed your pet some kale. Your pet now weighs", weight, "kg.")
+          return weight
+        elif feed_input == 2:
+          weight = weight + 0.2
+          print("You fed your pet some broccoli. Your pet now weighs", weight, "kg.")
+          return weight
+        elif feed_input == 3:
+          weight = weight + 0.4
+          print("You fed your pet some apples. Your pet now weighs", weight, "kg.")
+          return weight
+    except ValueError:
+      print("Please enter a valid number.")
+
+
+# MAIN PROGRAM
 print("Welcome to the Pet Simulator!")
 user_name = input("What is your name? ")
 pet_name = input("What would you like to name your pet? ")
 print("Hello {}! Your pet {} is ready for you!".format(user_name, pet_name))
 #add art here
 
+weight = 2
 selection = main_menu()
+weight = user_selection(selection, weight)
 
 
 '''
