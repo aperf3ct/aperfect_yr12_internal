7d7a27072b40b148f3ddcb1123d423c5d2ae0f59
aperf3ct <158367984+aperf3ct@users.noreply.github.com>
2024-04-26 22:44:31 +1200

    Update main.py
    check pet welbeing + minor personalisation changes

--- main.py
+++ main.py
@@ -6,37 +6,14 @@
 # if pet is well, pet will live
 
 '''
-25/04/2024
-10:20pm
-established main menu, feed function and play function.
-'''
-# FUNCTIONS
-def main_menu(weight):
-  Valid = False
-  while not Valid:
-    print("\nWelcome {}! What would you like to do?".format(user_name))
-    print("1. Feed")
-    print("2. Play")
-    print("3. Edit Names")
-    print("4. Quit")
-    try:
-      menu_input = int(input("\nEnter your choice: "))
-      if menu_input < 1 or menu_input > 4:
-        print("Please enter a valid number.")
-      elif menu_input == 1:
-        weight = feed(weight)
-      elif menu_input == 2:
-        weight = play(weight)
-      elif menu_input == 3:
-        edit_names()
-      elif menu_input == 4:
-        quit()
-    except ValueError:
-      print("Please enter a valid number.")
+26/04/2024
+4:00pm
 
+'''
+# FUNCTIONs 
 def feed(weight):
-  print("What woud you like to feed your pet?")
-  print("1. Kale\n2. Broccoli\n3. Apples")
+  print("\nWhat woud you like to feed", pet_name, "?")
+  print("1. Milk\n2. Biscuits\n3. Meat")
   Valid = False
   while not Valid:
     try:
@@ -47,22 +24,28 @@ def feed(weight):
         Valid = True
         if feed_input == 1:
           weight = weight + 1
-          print("You fed your pet some kale. Your pet now weighs", weight, "kg.")
+          print("\nYou fed {} some milk. {} now weighs {}kg.".format(
+            pet_name, pet_name, weight)
+          )
           return weight
         elif feed_input == 2:
           weight = weight + 2
-          print("You fed your pet some broccoli. Your pet now weighs", weight, "kg.")
+          print("\nYou fed {} some biscuits. {} now weighs {}kg.".format(
+          pet_name, pet_name, weight)  
+          )
           return weight
         elif feed_input == 3:
           weight = weight + 3
-          print("You fed your pet some apples. Your pet now weighs", weight, "kg.")
+          print("\nYou fed {} some Meat. {} now weighs {}kg.".format(
+          pet_name, pet_name, weight)
+          )
           return weight
     except ValueError:
       print("Please enter a valid number.")
 
 
 def play(weight):
-  print("What would you like to do with your pet?")
+  print("\nWhat would you like to do with ", pet_name, "?")
   print("1. Play fetch\n2. Play with a toy\n3. Play with a ball")
   Valid = False
   while not Valid:
@@ -74,29 +57,100 @@ def play(weight):
         Valid = True
         if play_input == 1:
           weight -= 3
-          print("You played fetch with your pet. Your pet now weighs", weight, "kg.")
+          print("\nYou played fetch with {}. {} now weighs {}kg.".format(
+            pet_name, pet_name, weight)
+          )
           return weight
         elif play_input == 2:
           weight -= 2
-          print('You played with a toy with your pet. Your pet now weighs', weight, 'kg.')
+          print("\nYou played with a toy with {}. {} now weighs {}kg.".format(
+            pet_name, pet_name, weight)
+          )
           return weight
         elif play_input == 3:
           weight -= 1
-          print("You played with a ball with your pet. Your pet now weighs", weight, "kg.")
+          print("\nYou played with a ball with {}. {} now weighs {}kg.".format(
+              pet_name, pet_name, weight)
+          )
           return weight
     except ValueError:
       print("Please enter a valid number.")
+
+
+def check_wellbeing(weight):
+  if weight < 1:
+    print(pet_name, "didn't eat in a while. They died ):")
+    quit()
+  elif weight > 10:
+    print(pet_name, "ate too much. They died ):")
+    quit()
+  elif weight < 4:
+    print(pet_name, "looks hungry.")
+  elif weight > 7:
+    print(pet_name, "looks full.")
+  elif weight in range (4,8):
+    print(pet_name, "is doing well :)")
   
 # MAIN PROGRAM
+print('''
+ |\_/|    
+ (. .)
+  =w= (\  
+ / ^ \//  
+(|| ||)
+,""_""_ .
+''')
+
+print('''
+               _ |\_      
+               \` ..\  
+          __,.-" =__Y=
+        ."        )
+  _    /   ,    \/\_  _ 
+ ((____|    )_-\ \_-`(_)
+ `-----'`-----` `--`
+''')
+
+print('''
+　　　　　／＞　　フ
+　　　　　| 　_　 _ l
+　 　　　／` ミ＿xノ
+　　 　 /　　　 　 |
+　　　 /　 ヽ　　 ﾉ
+　 　 │　　| | |
+''' )
+
+
 print("Welcome to the Pet Simulator!")
 user_name = input("What is your name? ")
 pet_name = input("What would you like to name your pet? ")
 print("Hello {}! Your pet {} is ready for you!".format(user_name, pet_name))
 #add art here
 
-weight = 2
-main_menu(weight)
-
+weight = 5
+Valid = False
+while not Valid:
+  print("\nWelcome {}! What would you like to do?".format(user_name))
+  print("1. Feed")
+  print("2. Play")
+  print("3. Edit Names")
+  print("4. Quit")
+  try:
+    menu_input = int(input("\nEnter your choice: "))
+    if menu_input < 1 or menu_input > 4:
+      print("Please enter a valid number.")
+    elif menu_input == 1:
+      weight = feed(weight)
+      check_wellbeing(weight)
+    elif menu_input == 2:
+      weight = play(weight)
+      check_wellbeing(weight)
+    elif menu_input == 3:
+      edit_names()
+    elif menu_input == 4:
+      quit()
+  except ValueError:
+    print("Please enter a valid number.")
 
 '''
 file = open("pet.txt", "w")
